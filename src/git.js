f8df3aa4bc6f281e372b3305c45f69c4728d5ee3
Daniel Del Core <daniel.delcore.92@gmail.com>
2024-01-22 21:56:42 +1100

    scaffolding

---
+++ src/git.js
@@ -0,0 +1,48 @@
+import { exec, getExecOutput } from "@actions/exec";
+
+export const setupUser = async () => {
+  await exec("git", ["config", "user.name", `"github-actions[bot]"`]);
+  await exec("git", [
+    "config",
+    "user.email",
+    `"github-actions[bot]@users.noreply.github.com"`,
+  ]);
+};
+
+export const pullBranch = async (branch) =>
+  await exec("git", ["pull", "origin", branch]);
+
+export const push = async (branch, { force } = {}) => {
+  await exec(
+    "git",
+    ["push", "origin", `HEAD:${branch}`, force && "--force"].filter(Boolean)
+  );
+};
+
+export const pushTags = async () =>
+  await exec("git", ["push", "origin", "--tags"]);
+
+export const switchToMaybeExistingBranch = async (branch) => {
+  const { stderr } = await getExecOutput("git", ["checkout", branch], {
+    ignoreReturnCode: true,
+  });
+  const isCreatingBranch = !stderr
+    .toString()
+    .includes(`Switched to a new branch '${branch}'`);
+  if (isCreatingBranch) {
+    await exec("git", ["checkout", "-b", branch]);
+  }
+};
+
+export const reset = async (pathSpec, mode = "hard") =>
+  await exec("git", ["reset", `--${mode}`, pathSpec]);
+
+export const commitAll = async (message) => {
+  await exec("git", ["add", "."]);
+  await exec("git", ["commit", "-m", message]);
+};
+
+export const checkIfClean = async () => {
+  const { stdout } = await getExecOutput("git", ["status", "--porcelain"]);
+  return !stdout.length;
+};
--- src/index.js
+++ src/index.js
@@ -1,24 +1,78 @@
 const core = require("@actions/core");
-const { exec } = require("child_process");
+const { exec, getExecOutput } = require("@actions/exec");
+
+const gitUtils = require("./git");
+
+function generatePr({ branch }) {
+  const repo = `${github.context.repo.owner}/${github.context.repo.repo}`;
+  const branch = github.context.ref.replace("refs/heads/", "");
+  const transformBranch = `hypermod-transform/hello`;
+
+  await gitUtils.switchToMaybeExistingBranch(transformBranch);
+  await gitUtils.reset(github.context.sha);
+
+  return '1';
+}
 
 (async () => {
-    const { transformIds, directories } = JSON.parse(core.getInput("data"));
+  const { transformIds, directories } = JSON.parse(core.getInput("data"));
 
-    core.info("Fetching and running provided transforms", transformIds, directories);
-    exec(
-      `npx @hypermod/cli -t ${transformIds} ${directories}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          core.setFailed(`Error: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          core.setFailed(`Error: ${stderr}`);
-          return;
-        }
-        core.setOutput("result", stdout);
-      }
-    );
+  // TODO: check if this is necessary
+  core.info("Setting GitHub credentials");
+
+  const githubToken = process.env.GITHUB_TOKEN;
+
+  if (!githubToken) {
+    core.setFailed("Please add the GITHUB_TOKEN to the hypermod workflow file");
+    return;
+  }
+
+  await fs.writeFile(
+    `${process.env.HOME}/.netrc`,
+    `machine github.com\nlogin github-actions[bot]\npassword ${githubToken}`
+  );
+
+  core.info(
+    "Fetching and running provided transforms",
+    transformIds,
+    directories
+  );
+
+  const { exitCode, stdout, stderr } = await getExecOutput(
+    "npx @hypermod/cli",
+    [`t=${transformIds.join(",")}`, directories]
+  );
+
+  if (exitCode) {
+    core.setFailed(`Error: ${error.message}`);
+    return;
+  }
+
+  if (stderr) {
+    core.setFailed(`Error: ${stderr}`);
+    return;
+  }
+
+  core.info("stdout", stdout);
+  // core.setOutput("result", stdout);
+
+  // TODO: perform formatting with script of choice via npm run hypermod:format
+  const pullRequestNumber = generatePR({ branch: 'main' });
+
+  // Check if there are any file diffs
+
+  // If no diffs, exit
+  if (false) {
+    core.info("No changes detected");
+    return;
+  }
+
+  // If so, generate pull requests
+  if () {
+    core.setOutput("pullRequestNumber", String(pullRequestNumber));
+    return
+  }
+
 })().catch((err) => {
   core.error(err);
   core.setFailed(err.message);
