ada4dd73cb073c7880d72da6182b72eaa139b38e
Daniel Del Core <daniel.delcore.92@gmail.com>
2024-07-21 15:25:08 +1000

    Patch transform.ts file support

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Github Action for progressing batch transformation requests triggered remotely from the Hypermod App.
 
-The action accepts a single argument `deploymentId`, which it will use to access specific transform instructions from the Hypermod App.
+The action accepts a single argument `deploymentId` & `deploymentKey`, which it will use to access specific transform instructions from the Hypermod App.
--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,10 @@ const HYPERMOD_DIR = ".hypermod";
   Object.entries(deployment.transforms).forEach(([id, sources]) => {
     sources.forEach((source) => {
       const filePath = path.join(process.cwd(), HYPERMOD_DIR, id, source.name);
-      if (filePath.includes("transform.js")) {
+      if (
+        filePath.includes("transform.ts") ||
+        filePath.includes("transform.js")
+      ) {
         transformPaths.push(filePath);
       }
 
@@ -82,6 +85,7 @@ const HYPERMOD_DIR = ".hypermod";
   });
 
   try {
+    // TODO: remove src path hardcoding
     const { exitCode, stderr } = await getExecOutput(
       `npx --yes @hypermod/cli -t ${transformPaths.join(
         ","
